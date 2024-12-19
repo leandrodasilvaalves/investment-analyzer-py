5ee5915059b02bf94d5f479e43a0632c4e08b968
Leandro Da Silva Alves <lalves@acesso.com>
2024-12-19 16:34:58 -0300

    Dockerizar applicação

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-# from correlation import correlation as cor
+from correlation import correlation as cor
 
-# print(cor.calculate("CSNA3", "CPLE6"))
+print(cor.calculate("PETR4", "VALE3"))
 
-from formulas import bazins_formula as bazin
+# from formulas import bazins_formula as bazin
 
-bazin.process_formula(["PETR4", "PETR3", "VALE3", "ITUB4", "BBAS3", "AAPL34", "AGRO3", "AGRO3F"])
+# bazin.process_formula(["PETR4", "PETR3", "VALE3", "ITUB4", "BBAS3", "AAPL34", "AGRO3", "AGRO3F"])
